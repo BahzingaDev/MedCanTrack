cbe2b4f48436296a6eec5bb9b5f5d6ef3bfdbf2f
Jack Carter <jackcarter.developer@gmail.com>
2025-05-01 10:21:03 +0100

    Delete src directory

--- src/models/cannabinoid.ts
+++
@@ -1,9 +0,0 @@
-export class Cannabinoid {
-    name: string;
-    concentration: number;
-
-    constructor(name: string, concentration: number) {
-        this.name = name;
-        this.concentration = concentration;
-    }
-}
--- src/models/terpene.ts
+++
@@ -1,9 +0,0 @@
-export class Terpene {
-    name: string;
-    concentration: number;
-
-    constructor(name: string, concentration: number) {
-        this.name = name;
-        this.concentration = concentration;
-    }
-}
